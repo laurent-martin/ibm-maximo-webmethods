142e6a2a9b713ef1facf399d55fbaf95c64bd880
TMilliard-sag <thierry.milliard@ibm.com>
2026-03-31 16:45:13 +0200

    First commit with assets descriptions
    Adding some assets. First part.

---
+++ Form to initiate workflow/StartProxy.bat
@@ -0,0 +1,2 @@
+cd C:\Users\ThierryMilliard\OneDrive - IBM\Documents\IWHI\Demo Maximo - Iwhi\Assets démo
+node proxy.js
---
+++ Form to initiate workflow/StartServer.bat
@@ -0,0 +1,2 @@
+cd C:\Users\ThierryMilliard\OneDrive - IBM\Documents\IWHI\Demo Maximo - Iwhi\Assets démo
+ npx serve -p 4000 service_request_form.html
